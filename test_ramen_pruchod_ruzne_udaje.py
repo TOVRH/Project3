df6c43a717fc9e5a416e9676caa2adc0221516a9
TOVRH <vrhel.t@gmail.com>
2026-02-08 21:02:49 +0100

    2.2

--- test_ramen_pruchod_ruzne_udaje.py
+++ test_ramen_pruchod_ruzne_udaje.py
@@ -12,21 +12,41 @@ from playwright.sync_api import Page, expect
 def test_ramen(page: Page, auto_accept_cookies, first_steps, name: str, phone: str, email: str, error_count: int):
     page.get_by_role("button",name="Pokračovat").click()
 
-    expect(page.locator('[data-element="order-customer-info_name-input"]')).to_be_visible(timeout=5000)
-    page.fill('[data-element="order-customer-info_name-input"]', name)
-    expect(page.locator('[data-element="order-customer-info_phone-input"]')).to_be_visible(timeout=5000)
-    page.fill('[data-element="order-customer-info_phone-input"]', phone)
-    expect(page.locator('[data-element="order-customer-info_email-input"]')).to_be_visible(timeout=5000)
-    page.fill('[data-element="order-customer-info_email-input"]', email)
+    closed = page.get_by_text("Upozorňujeme, že momentálně neprovozujeme.")
+    name_input = page.locator('[data-element="order-customer-info_name-input"]')
+    phone_input = page.locator('[data-element="order-customer-info_phone-input"]')
+    email_input = page.locator('[data-element="order-customer-info_email-input"]')
+    errors = page.locator(".styles_error__PL_mX, .styles_PhoneFieldError__jswNz")
+
+
+    expect(name_input.or_(closed)).to_be_visible(timeout=5000)
+
+    if closed.is_visible():
+        return
+    
+    expect(name_input).to_be_visible()
+    expect(phone_input).to_be_visible()
+    expect(email_input).to_be_visible()
+
+    name_input.fill(name)
+    phone_input.fill(phone)
+    email_input.fill(email)
 
     page.get_by_role("button", name="Potvrdit").click()
     
-    errors = page.locator(".styles_error__PL_mX, .styles_PhoneFieldError__jswNz")
-
     if error_count > 0:
+        expect(
+            errors.or_(closed)
+        ).to_be_visible(timeout=5000)
+
+        if closed.is_visible():
+            expect(closed).to_be_visible()
+            return
+
         expect(errors).to_have_count(error_count, timeout=5000)
+
     else:
         expect(
             page.get_by_text("Způsob platby")
-            .or_(page.get_by_text("Upozorňujeme, že momentálně neprovozujeme."))
-        ).to_be_visible(timeout=5000)
+            .or_(closed)
+        ).to_be_visible (timeout=5000)
